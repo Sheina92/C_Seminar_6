02a27f67ba3aa7530010802bb9edaadd0043a0cc
Ирина <ira.sheina.92@gmail.com>
2023-10-26 08:54:16 +0300

    Добавлено решение Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+System.Console.Write("Введите количество чисел: ");
+int value = int.Parse(System.Console.ReadLine());
+
+int count = 0;
+
+for(int i = 0; i < value; i++)
+{
+    System.Console.Write("Введите число: ");
+    if(int.Parse(System.Console.ReadLine()) > 0 ) count++;
+}
+
+System.Console.WriteLine("Чисел больше нуля: " + count);
